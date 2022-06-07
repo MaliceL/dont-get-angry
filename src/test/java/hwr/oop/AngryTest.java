0430c13b1014019f9f5d45613db10ffc4ace4d99
MaliceL <niklas.lischke@gmx.de>
2022-06-07 15:33:07 +0200

    update 07.06.2022

--- src/test/java/hwr/oop/AngryTest.java
+++ src/test/java/hwr/oop/AngryTest.java
@@ -1,7 +1,5 @@
 package hwr.oop.angry.game;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
